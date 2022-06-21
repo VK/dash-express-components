2b1e4b477b3d95e1bbce990a881cf66cc78d7953
VK <viktor@krueckl.de>
2022-06-21 09:15:09 +0200

    retry eval with python, if numexpr does not work

--- dash_express_components_base/transformationtypes/_eval.py
+++ dash_express_components_base/transformationtypes/_eval.py
@@ -24,8 +24,13 @@ def compute(cfg, inputDataFrame):
         "c": 2.99792458e17,
         "hbar_c": 197.326979
     }
-    inputDataFrame[cfg["col"]] = inputDataFrame.eval(
-        formula, local_dict=var_dict, engine='numexpr')
+
+    try:
+        inputDataFrame[cfg["col"]] = inputDataFrame.eval(
+            formula, local_dict=var_dict, engine='numexpr')
+    except:
+        inputDataFrame[cfg["col"]] = inputDataFrame.eval(
+            formula, local_dict=var_dict, engine='python')
 
     # make the back transformation
     inputDataFrame.rename(
