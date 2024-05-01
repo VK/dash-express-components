f050e69e47948695278eda6c00438d32a72c005c
VK <viktor@krueckl.de>
2024-05-01 17:15:35 +0200

    release

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 import json
 import os
-from setuptools import setup, find_packages
-
+from setuptools import setup
 
 
 with open('package.json') as f:
@@ -9,15 +8,21 @@ with open('package.json') as f:
 
 package_name = package["name"].replace(" ", "_").replace("-", "_")
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name=package_name,
+    url="https://github.com/VK/dash-express-components",
     version=package["version"],
     author=package['author'],
-    packages=[p for p in find_packages() if p.startswith(package_name)],
+    packages=[package_name],
     include_package_data=True,
     license=package['license'],
     description=package.get('description', package_name),
-    install_requires=[],
+    long_description= long_description,
+    long_description_content_type="text/markdown",    
+    install_requires=["dash"],
     classifiers = [
         'Framework :: Dash',
     ],    
