474d17e3e5f846ee6e92bf73cec3765f10a7af9a
Viktor Krückl <viktor.krueckl@ams-osram.com>
2025-03-11 08:29:52 +0100

    release

--- dash_express_components_base/transformationtypes/_rename.py
+++ dash_express_components_base/transformationtypes/_rename.py
@@ -1,4 +1,9 @@
 def compute(cfg, inputDataFrame):
+
+    # remove the columns we want to rename to avoid conflicts
+    to_remove = [c for c in inputDataFrame.columns if c in cfg["columns"].values() and c not in cfg["columns"].keys()]
+    inputDataFrame = inputDataFrame.drop(columns=to_remove)
+
     return inputDataFrame.rename(columns=cfg["columns"])
 
 def dimensions(cfg, inputDataFrame):
